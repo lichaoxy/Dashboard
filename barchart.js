f4e45cb545c2636602a98eccc5f9b647d3957047
lichaoxy <lichaoxy@126.com>
2014-12-14 22:55:37 +0800

    饼图带标签

--- barchart.js
+++ barchart.js
@@ -1,20 +1,20 @@
-﻿function barchart(data){
-   var margin={top:30,bottom:30,left:30,right:30};
+﻿function barchart(bardata){
+   var margin={top:30,bottom:30,left:50,right:30};
  //图形的高度和宽度
-   var width=800-margin.left-margin.right;
+   var width=700-margin.left-margin.right;
    var height=300-margin.top-margin.bottom;
-    var svg=d3.select('#barchart1').append("svg").attr("width",700).attr("height",350)
+    var svg=d3.select('#barchart1').append("svg").attr("width",600).attr("height",350)
         .append("g")
         .attr("transform","translate("+margin.left+","+margin.top+")") ; //向下和右平移一段距离，
                                                                                                             // 但是在计算坐标的时候还是要从左上角原点为起点计算
  //设置X方向上的显示比例
     var xScale = d3.scale.ordinal()
         .rangeRoundBands([0, width],.3) //坐标轴描点的间隔
-        .domain(data.map(function(d,i){ return d.key;})); //x轴坐标显示的内容
+        .domain(bardata.map(function(d,i){ return d.key;})); //x轴坐标显示的内容
    
  //设置Y方向的显示比例
     var yScale = d3.scale.linear()
-          .domain([0,d3.max(data,function(d){return d.values;})])
+          .domain([0,d3.max(bardata,function(d){return d.values;})])
           .rangeRound([height,0]); //如果是[0,height]坐标轴的数据显示就是从上往下从小到大，
    
  //设置x轴
@@ -36,10 +36,11 @@
        .call(yaxis);
  //画柱子
     svg.selectAll("rect")
-       .data(data)
+       .data(bardata)
        .enter()
        .append("rect")
        .on("mouseover",mouseover)
+	   .on("click",barclick)
        .attr("fill","red")
        .attr("y",function(d){return yScale(d.values);}) //计算 柱子的起始坐标。 x,y表示柱子的左上角坐标
        .attr("x",function(d){return xScale(d.key);})
@@ -55,5 +56,17 @@
  
    function mouseover(d){
    tip.show(d);
+   }
+   function barclick(d){
+	var piedata=d3.nest()
+		          .key(function(d){return d[0];})						  
+		          .rollup(function(values){
+		          		return d3.sum(values,function(v){
+		          					if (v[1]==d.key) 
+										return v[2];
+									});})
+		          .entries(data);
+	var piedata_ori=data.filter(function (v){ if (v[1]==d.key) return v;});
+	piechart(piedata,piedata_ori);
    }
 }
--- piechart.js
+++ piechart.js
@@ -1,15 +1,74 @@
-function piechart(pD){
-var segColor=['#7373B9','#9F4D95','#467500','#0000C6','#00AEAE','#4EFEB3','#009100','#BB3D00','#B7FF4A','#FFD306','#613030','#AFAF61','#4F9D9D','#6C3365'];
+function piechart(piedata,piedata_ori){
+ var segColor=['#807dba','#e08214','#FF83FA','#41ab5d','#EECBAD','#4EFEB3','#009100','#BB3D00','#B7FF4A','#FFD306','#613030','#AFAF61','#4F9D9D','#6C3365'];
+ var textOffset = 20;
  d3.select('#piechart1 svg').remove();
- var svgdim={w:400,h:350};
-    var pieDim ={w:250, h: 250};
+ var total=d3.sum(piedata,function(d){return d.values;});
+ var svgdim={w:300,h:350};
+    var pieDim ={w:125, h: 125};
     pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+	var r = Math.min(pieDim.w, pieDim.h) / 2;
     var piesvg = d3.select('#piechart1').append("svg")
         .attr("width", svgdim.w).attr("height", svgdim.h)
-        .attr("transform", "translate("+pieDim.w/2+","+svgdim.h/2+")");
-    var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(0);
+        .attr("transform", "translate("+svgdim.w/2+","+svgdim.h/3+")");
+    var arc = d3.svg.arc().outerRadius(pieDim.r).innerRadius(pieDim.r/2-10);
     var pie = d3.layout.pie().sort(null).value(function(d) { return d.values; });
-  piesvg.selectAll("g")
-		.data(pie(pD)).enter().append("path").attr("d", arc)
-        .style("fill", function(d,i) {return segColor[i];});
+		piesvg.selectAll("g")
+		     .data(pie(piedata)).enter().append("path").attr("d", arc)
+             .style("fill", function(d,i) {return segColor[i];});
+  
+    var label_group = piesvg.append("svg:g")
+    .attr("class", "label_group");
+	var lines = label_group.selectAll("line").data(pie(piedata));
+      lines.enter().append("svg:line")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", -pieDim.r)
+        .attr("y2", -pieDim.r-18)
+        .attr("stroke", "gray")
+        .attr("transform", function(d) {
+          return "rotate(" + (d.startAngle+d.endAngle)/2 * (180/Math.PI) + ")";
+        });
+ //DRAW LABELS WITH PERCENTAGE VALUES
+     var valueLabels = label_group.selectAll("text.value").data(pie(piedata))
+        .attr("dy", function(d){
+          if ((d.startAngle+d.endAngle)/2 > Math.PI/2 && (d.startAngle+d.endAngle)/2 < Math.PI*1.5 ) {
+            return 5;
+          } else {
+            return -7;
+          }
+        })
+        .attr("text-anchor", function(d){
+          if ( (d.startAngle+d.endAngle)/2 < Math.PI ){
+            return "beginning";
+          } else {
+            return "end";
+          }
+        })
+        .text(function(d){
+          var percentage = (d.value/100)*100;
+          return percentage.toFixed(1) + "%";
+        });
+
+      valueLabels.enter().append("svg:text")
+        .attr("class", "value")
+        .attr("transform", function(d) {
+          return "translate(" + Math.cos(((d.startAngle+d.endAngle - Math.PI)/2)) * (r+textOffset) + "," + Math.sin((d.startAngle+d.endAngle - Math.PI)/2) * (r+textOffset) + ")";
+        })
+        .attr("dy", function(d){
+          if ((d.startAngle+d.endAngle)/2 > Math.PI/2 && (d.startAngle+d.endAngle)/2 < Math.PI*1.5 ) {
+            return 5;
+          } else {
+            return -7;
+          }
+        })
+        .attr("text-anchor", function(d){
+          if ( (d.startAngle+d.endAngle)/2 < Math.PI ){
+            return "beginning";
+          } else {
+            return "end";
+          }
+        }).text(function(d){
+          var percentage = (d.value/total)*100;
+          return percentage.toFixed(1) + "%";
+        });		
  }
